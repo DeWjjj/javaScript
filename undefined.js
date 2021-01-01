310e56aa8ff2147a3d969a470a2b4634e41176d4
DeWjjj <wangchaonew123@qq.com>
2021-01-01 17:16:23 +0800

    Create undefined.js

---
+++ undefined.js
@@ -0,0 +1,10 @@
+let message;
+console.log(typeof message); //undefined
+console.log(typeof age); //undefined
+
+if (!message) {
+    console.log("From if (!message).")
+}
+// if (age) {
+//     console.log("From if (age).")
+// } //This if will is an error.
