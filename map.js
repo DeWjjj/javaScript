8efab3990cdc2fcc223455b1016fca1363fe602f
DeWjjj <wangchaonew123@qq.com>
2021-01-03 07:11:51 +0800

    main

--- map.js
+++ map.js
@@ -1,2 +1,6 @@
 let array = [1,2,3];
-console.log(array.map((e, i)=>`${e}${array[i]}`));
+console.log(array.map((x, y,z)=>`${x}${y} ${z}`));
+array.map(function (val,ind,fullVal){
+    console.log(`${val}${ind} ${fullVal}`)
+})
+//map(function(val,index,fullVal){}
---
+++ unicode.js
@@ -0,0 +1,13 @@
+console.log(`\u00A9`);
+console.log(String.raw`\u00A9`);
+
+console.log(`first line\nsecond line`);
+//first line
+//second line
+
+console.log(`first line
+second line`);
+
+console.log(String.raw`first line\nsecond line`);
+//first line\nsecond line
+
