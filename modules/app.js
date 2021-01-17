da4c71d96df96949ed52e6d0545936b74b43f2d7
DeWjjj <wangchaonew123@qq.com>
2021-01-17 21:36:05 +0800

    Summary

---
+++ modules/app.js
@@ -0,0 +1,6 @@
+import * as module1 from "./module1.js";
+import * as module2 from "./module2.js";
+import * as module3 from "./module3.js";
+console.log(module1);
+console.log(module2);
+console.log(module3);
